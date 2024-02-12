a4d2f43350987a0a8e1ba7f400b267d922f5a3d7
zhenouth <zhenouth@stud.ntnu.no>
2024-02-12 15:04:59 +0100

    Dashboard layout

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -19,7 +19,7 @@ export default function Dashboard() {
         <meta name="dashboard" content="Learn more about what IceBreakr offers." />
       </Head>
 
-      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+      <main className="flex font-darker min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
         <SignedOut>
           <SignInButton />
           <p>
@@ -31,14 +31,20 @@ export default function Dashboard() {
             <h1>Dashboard</h1>
             <div className='flex w-full max-w-screen-xl h-screen bg-violet-500 rounded-3xl'>
                 <section className='rounded-2xl flex-col w-1/4 bg-lime-500 flex align-middle justify-center p-0 m-2'>
-                    <div className='rounded-2xl w-full h-full max-h-40 bg-slate-500 flex align-middle justify-center p-2 mb-2'>USERBOX</div>
-                    <div className='rounded-2xl w-full h-full bg-teal-500 flex align-middle justify-center p-2' >PLAYLIST</div>
+                    <div className='rounded-2xl w-full h-full max-h-40 bg-slate-500 flex flex-col align-middle justify-center p-2 mb-2'>
+                        <button className='w-full h-full hover:bg-lime-500'>User</button>
+                        <button className='w-full h-full hover:bg-rose-500'>Home</button>
+                    </div>
+                    <div className='rounded-2xl w-full h-full bg-teal-500 flex align-middle justify-center p-2' >
+                        PLAYLIST
+                    </div>
                 </section>
                 <section className='rounded-2xl w-1/2 flex align-middle justify-center flex-row bg-teal-800 p-8 mt-2 mb-2'>
                     <div><h1>Mid</h1></div>
                 </section>
                 <section className='rounded-2xl w-1/4 flex align-middle justify-center bg-rose-700 p-8 m-2'>
-                    Rightgh
+                    <div className='rounded-2xl w-full h-full max-h-40 bg-slate-500 flex align-middle justify-center p-2 mb-2'>USERBOX</div>
+                    <div className='rounded-2xl w-full h-full bg-teal-500 flex align-middle justify-center p-2' >PLAYLIST</div>
                 </section>
             </div>
 
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,7 +6,7 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        sans: ["var(--font-sans)", ...fontFamily.sans],
+        darker: ['Darker Grotesque', ...fontFamily.sans]
       },
     },
   },
