129232a4d6d2575c4081db8467eeeb1e2dbf5610
zhenouth <zhenouth@stud.ntnu.no>
2024-02-15 13:01:24 +0100

    file path

--- src/components/advertisement.tsx
+++ src/components/advertisement.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import React from "react";
-import ExampleAd from '~/assets//images/eucalyptus-oil-ad.png';
+import ExampleAd from '~/assets/images/eucalyptus-oil-ad.png';
 
 const Advertisement: React.FC = () => {
     return (
