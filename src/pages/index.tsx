fe2878a3115a0dc5adb6eef3d2e22d7f1f334936
Mats Alexander Nissen-Lie <matsani@stud.ntnu.no>
2024-03-11 12:27:30 +0100

    lint

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -36,7 +36,7 @@ export default function Home() {
   }, [isLoading, isSuccess, router, useUserMuation, user, userDb]);
 
   return (
-    <div className="flex h-full w-full items-center justify-center">
+    <div className="flex h-screen w-full items-center justify-center">
       <h1 className=" text-white">Loading</h1>
     </div>
   );
